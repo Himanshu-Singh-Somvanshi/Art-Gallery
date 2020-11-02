43f6d8c2e3b0b8f0c3505a5061ff43e26f9beef6
LunarLeo <55891275+LunarLeo@users.noreply.github.com>
2020-11-02 06:17:23 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * Pinterest-like script - a series of tutorials
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2012, Script Tutorials
+ * http://www.script-tutorials.com/
+ */
+
+$(document).ready(function(){
+
+    // masonry initialization
+    $('.main_container').masonry({
+        // options
+        itemSelector : '.pin',
+        isAnimated: true,
+        isFitWidth: true
+    });
+
+    // onclick event handler (for comments)
+    $('.comment_tr').click(function () {
+        $(this).toggleClass('disabled');
+        $(this).parent().parent().parent().find('form').slideToggle(250, function () {
+            $('.main_container').masonry();
+        });
+    }); 
+
+});
