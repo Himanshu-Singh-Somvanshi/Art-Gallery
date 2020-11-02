2f857ab968919cbb4a37b2bead344a7d775f2077
Himanshu Singh <55891275+LunarLeo@users.noreply.github.com>
2020-11-02 07:32:37 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
-
-    // masonry initialization
     $('.main_container').masonry({
         // options
         itemSelector : '.pin',
         isAnimated: true,
         isFitWidth: true
     });
-
-    // onclick event handler (for comments)
     $('.comment_tr').click(function () {
         $(this).toggleClass('disabled');
         $(this).parent().parent().parent().find('form').slideToggle(250, function () {
